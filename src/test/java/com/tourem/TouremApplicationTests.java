45f3ca5b572c6dcad388e10cd6d2fd2b303e1f42
Mohamed Toure <demahom.toure@gmail.com>
2021-12-06 21:04:51 +0100

    initial commit

---
+++ src/test/java/com/tourem/TouremApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouremApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
