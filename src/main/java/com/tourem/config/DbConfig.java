a439ac770545d71e835efe2ca7e564faebf9edf5
Mohamed Toure <demahom.toure@gmail.com>
2022-02-21 16:50:56 +0100

    hide password in author dto

--- src/main/java/com/tourem/config/DbConfig.java
+++ src/main/java/com/tourem/config/DbConfig.java
@@ -5,9 +5,11 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
+@Profile("!dev")
 @Configuration
 public class DbConfig {
 
--- src/main/java/com/tourem/mappers/AuthorMapper.java
+++ src/main/java/com/tourem/mappers/AuthorMapper.java
@@ -3,7 +3,11 @@ package com.tourem.mappers;
 import com.tourem.dao.entities.AuthorEntity;
 import com.tourem.dto.AuthorDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
-public interface AuthorMapper extends TouremObjectMapper<AuthorEntity, AuthorDto>{
+public interface AuthorMapper extends TouremObjectMapper<AuthorEntity, AuthorDto> {
+
+	@Mapping(target = "password",ignore = true)
+	AuthorDto mapToDto(AuthorEntity source);
 }
