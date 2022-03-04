178f99ca305a54a400e4c74414c98ed7ee80efa4
Mohamed Toure <demahom.toure@gmail.com>
2022-03-05 00:46:58 +0100

    allows all origin

--- src/main/java/com/tourem/controller/AbstractTouremController.java
+++ src/main/java/com/tourem/controller/AbstractTouremController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @Slf4j
-@CrossOrigin(origins = "${cors-origin}")
+@CrossOrigin(origins = "*")
 public abstract class AbstractTouremController<D extends TouremDto> implements TouremController<D> {
 
 	protected final TouremService<D> service;
